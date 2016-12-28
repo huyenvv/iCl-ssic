2607ba95610d63c7c11e405d1941c8358a8cfa22
HuyenVV <Huyenvv.it@gmail.com>
2016-12-28 14:20:19 +0700

    Create

---
+++ iClassic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iClassic.Startup))]
+namespace iClassic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
